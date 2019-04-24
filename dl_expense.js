5590b1d558bb14d44a459cbf29387b90a90f7226
Emmanuel Cortes <ecortes016@west-mec.org>
2019-04-23 17:18:54 -0700

    Final Commit. Ready for Review
    Created a function that will calculate the total of the table row columns only if they are a digit, setting the value only two digits past the decimal

--- dl_expense.js
+++ dl_expense.js
@@ -33,7 +33,7 @@
 
 window.onload = function () {
       // Gathers all of the input elements that have the word 'sum' inside of the class attribute
-      var changingCells = document.querySelectorAll('input[class="sum"]');
+      var changingCells = document.querySelectorAll("table#travelExp input.sum");
 
       // Will loop through the changingCells collection and run the calcExp function when the user changes its focus
       for (var i = 0; i < changingCells.length; i++) {
@@ -44,34 +44,48 @@ window.onload = function () {
 }
 
 function validateSummary() {
+      // This variable will store the text box 
       var summary = document.getElementById("summary");
-
       // This if else statement will display a custom message that will display if the user forgets to type something in the text box
       if (summary.validity.valueMissing) {
+            // If the user forgets to fill out the text box then this message will appear
             summary.setCustomValidity("You must include a summary of the trip in your report.")
       } else {
+            // If the user fills out the text box then no message will show up
             summary.setCustomValidity("");
       }
 }
 
 function calcClass(sumClass) {
-      var sumFields = document.getElementsByClassName("sumClass");
+      // This variable will store all elements within the sumClass 
+      var sumFields = document.getElementsByClassName(sumClass);
+      // Set to an initial value of 0
       var sumTotal = 0;
 
       for (var i = 0; i < sumFields.length; i++) {
             var itemValue = parseFloat(sumFields[i].value);
+            // This if statement will determine if the itemValue variable is not not a number
             if (!isNaN(itemValue)) {
-                  itemValue += itemValue;
+                  // If the itemValue value is a digit the the itemValue value will be added onto the sumTotal value
+                  sumTotal += itemValue;
             }
       }
       return sumTotal;
 }
 
 function calcExp() {
+      // Will store all of the table row elements within the table
       var expTable = document.querySelectorAll("table#travelExp tbody tr");
       for (var i = 0; i < expTable.length; i++) {
-
+            document.getElementById("subtotal" + i).value = formatNumber(calcClass("date" + i), 2);
       }
+
+      // All of the following code will grab the elements value and set it equal to a number based on the formatNumber function
+      document.getElementById("transTotal").value = formatNumber(calcClass("trans"), 2);
+      document.getElementById("lodgeTotal").value = formatNumber(calcClass("lodge"), 2);
+      document.getElementById("mealTotal").value = formatNumber(calcClass("meal"), 2);
+      document.getElementById("otherTotal").value = formatNumber(calcClass("other"), 2);
+      document.getElementById("expTotal").value = formatUSCurrency(calcClass("sum"));
 }
 
 function formatNumber(val, decimals) {
